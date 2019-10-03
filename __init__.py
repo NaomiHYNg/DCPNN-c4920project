943f543707e75f4d236af362fd87dee63f372ea3
darrenz78 <darrenz78@hotmail.com>
2019-10-03 15:54:01 +1000

    Basic css styling with nav bar

--- __init__.py
+++ __init__.py
@@ -10,5 +10,13 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/history/')
+def history():
+    return render_template('history.html')
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
