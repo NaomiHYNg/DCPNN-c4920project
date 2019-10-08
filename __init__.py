675c40b92f3518c16ad5f82aa31fbc609e969982
darrenz78 <darrenz78@hotmail.com>
2019-10-08 20:18:57 +1100

    Added Login Screeen with a base username and password. username=admin password=password

--- __init__.py
+++ __init__.py
@@ -1,21 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
+global user
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'password':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return render_template('home.html', username=request.form['username'])
+    return render_template('login.html', error=error)
+
+@app.route('/home', methods=['GET', 'POST'])
 def home():
+
+    if request.method == 'POST':
+        return render_template('home.html', username=request.form['username'])
+
     return render_template('home.html')
 
-@app.route('/about/')
+@app.route('/about/', methods=['GET', 'POST'])
 def about():
+
+    if request.method == 'POST':
+        return render_template('about.html', username=request.form['username'])
+
     return render_template('about.html')
 
-@app.route('/history/')
+@app.route('/history/', methods=['GET', 'POST'])
 def history():
+
+    if request.method == 'POST':
+        return render_template('history.html', username=request.form['username'])
+
     return render_template('history.html')
 
-@app.route('/contact/')
+@app.route('/contact/', methods=['GET', 'POST'])
 def contact():
+
+    if request.method == 'POST':
+        return render_template('contact.html', username=request.form['username'])
+
     return render_template('contact.html')
 
 if __name__ == "__main__":
